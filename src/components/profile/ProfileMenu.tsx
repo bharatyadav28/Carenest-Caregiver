9a8964a78979b0d98047614199864e77bccc822c
Bharat Yadav <yadavbharat386@gmail.com>
2025-06-20 15:45:57 +0530

    Delete account and logout screens

--- src/components/profile/ProfileMenu.tsx
+++ src/components/profile/ProfileMenu.tsx
@@ -1,10 +1,34 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+
 import { sidebarItemType } from "@/lib/interface-types";
 import SidebarMenu from "../common/SidebarMenu";
+import ActionDialog from "../common/ActionDialog";
+import { binIconTheme, logoutIcon } from "@/lib/svg_icons";
 
 function ProfileMenu() {
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [openLogoutDialog, setOpenLogoutDialog] = useState(false);
+
+  const router = useRouter();
+
+  const handleDeleteDialog = () => {
+    setOpenDeleteDialog((prev) => !prev);
+  };
+
+  const handleLogoutDialog = () => {
+    setOpenLogoutDialog((prev) => !prev);
+  };
+
+  const handleLogout = () => {
+    Cookies.remove("care_giver_token");
+    handleLogoutDialog();
+    router.push("/signin");
+  };
+
   const items: sidebarItemType[] = [
     {
       id: 1,
@@ -29,15 +53,37 @@ function ProfileMenu() {
     {
       id: 5,
       text: "Delete Account",
-      handleClick: () => {},
+      handleClick: handleDeleteDialog,
     },
     {
       id: 6,
       text: "Log Out",
-      handleClick: () => {},
+      handleClick: handleLogoutDialog,
     },
   ];
-  return <SidebarMenu items={items} />;
+  return (
+    <>
+      <SidebarMenu items={items} />
+
+      <ActionDialog
+        open={openDeleteDialog}
+        handleOpen={handleDeleteDialog}
+        icon={binIconTheme}
+        heading="Account Deletion"
+        subheading="Are you sure you want to delete your account?"
+        handleConfirm={handleDeleteDialog}
+      />
+
+      <ActionDialog
+        open={openLogoutDialog}
+        handleOpen={handleLogoutDialog}
+        icon={logoutIcon}
+        heading="Account Logout"
+        subheading="Are you sure you want to logout your account?"
+        handleConfirm={handleLogout}
+      />
+    </>
+  );
 }
 
 export default ProfileMenu;
